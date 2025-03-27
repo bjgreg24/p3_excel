48ffcde31ab75c3f9075f49c789b0e0c9c4fe068
Luke <lukeaaronmiller@gmail.com>
2025-03-27 12:49:46 -0600

    Student Class File
    Creates a student class. Student objects have first name, last name, student ID, class name, and grade as attributes. This class also contains a method to format data from the initial excel files. (For instance, it will take "Miller_Luke_A12345Z" and set Miller to last name, Luke to first name, and A12345Z to student id).

---
+++ studentClass.py
@@ -0,0 +1,36 @@
+
+# Creates student class.
+class Student:
+    # Uses class name, student data string, and grade as parameters.
+    # Key class attributes are first name, last name, student id, class name, and grade.
+    def __init__(self, class_name, student_data, grade):
+        self.class_name = class_name
+        self.lname = ""
+        self.fname = ""
+        self.student_id = ""
+        self.student_data = student_data
+        self.unpackStudent(student_data)
+        self.grade = grade
+
+    # This method takes the string passed through student_data and splits it into each relevant attribute.
+    # for instance, it will take "Miller" from "Miller_Luke_A512093" and add it to the last name attribute.
+    def unpackStudent(self, student_data):
+        dataList = student_data.split("_")
+        self.fname = dataList[1]
+        self.lname = dataList[0]
+        self.student_id = dataList[2]
+
+
+# THIS CODE IS ONLY TO TEST THE CLASS AND OBJECTS.
+# It will not be used in the final program!!
+student_data = str(input("Enter student data string: "))
+className = str(input("Enter class name: "))
+grade = int(input("Enter grade: "))
+
+oStudent = Student(className, student_data, grade)
+
+print(oStudent.fname)
+print(oStudent.lname)
+print(oStudent.student_id)
+print(oStudent.class_name)
+print(oStudent.grade)
